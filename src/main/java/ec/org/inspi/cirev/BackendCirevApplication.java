16da30191bce6c792ca40be356c33375c7db33f9
levillagran <leninsasos@hotmail.com>
2022-02-16 21:26:00 -0500

    creacion proyecto

---
+++ src/main/java/ec/org/inspi/cirev/BackendCirevApplication.java
@@ -0,0 +1,13 @@
+package ec.org.inspi.cirev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCirevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCirevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/org/inspi/cirev/BackendCirevApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.org.inspi.cirev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCirevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
